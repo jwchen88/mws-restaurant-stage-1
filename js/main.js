1b69fb69747a7807807c3cee6414b72c77df047f
Jiawen Chen <sophiachen1130@gmail.com>
2018-10-28 02:07:31 -0500

    Add descriptive labels for View Details, register service worker

--- js/main.js
+++ js/main.js
@@ -178,7 +178,11 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
-  more.tabIndex = "3";
+  more.setAttribute(
+    "aria-label",
+    restaurant.name + "," + "restaurant.neighborhood"
+  );
+  more.tabIndex = "1";
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
@@ -210,3 +214,11 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     self.markers.push(marker);
   });
 } */
+
+if('serviceWorker' in navigator){
+  navigator.serviceWorker
+  .register('/sw.js')
+  .catch(function(err){
+    console.error(err);
+  })
+}
